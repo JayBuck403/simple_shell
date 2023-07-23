d8b0e281f5e2a263605e84d1e01a67885336bd34
Theo <jaybuck403@gmail.com>
2023-07-23 10:40:58 +0000

    Added execve function

--- exec_func.c
+++ exec_func.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
+/**
+ * exec_func - Implements the execve function
+ * @argv - pointer to pointer
+ *
+ * Return: void
+ */
+
 void exec_func(char **argv)
 {
 	char *command = NULL;
